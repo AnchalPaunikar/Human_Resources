ded939772896ef741b1ec8d0102bb6467f54c028
AnchalPaunikar <141146349+AnchalPaunikar@users.noreply.github.com>
2023-08-01 19:12:58 +0530

    hr.r

--- hr.r
+++ hr.r
@@ -6,51 +6,41 @@ library(gbm) # for gradient boosting
 library(cvTools) # for cv tuning
 library(xgboost) # for xgboost
 library(randomForest) 
-setwd("C:/Users/ASUS/OneDrive/Desktop/Edvancer R/R")
+#createDummies function
 source("createDummies.r")
-setwd("C:/Users/ASUS/OneDrive/Desktop/Edvancer R/R/projectdb")
 
+#Impoting data in R
 hr_train = read.csv("hr_train.csv")
 hr_test = read.csv("hr_test.csv", stringsAsFactors = T)
+
+#Adding target variable "left" in test data
 hr_test$left = NA
+
+#Adding new column to differentiate between train and test data after combining
 hr_test$data = "test"
 hr_train$data = "train"
 
-modi = read.csv("dimag.csv") 
-
-hr_train1 = read.csv("hr_train.csv")
-
+#Combining train and test data
 all_hr = rbind(hr_train,hr_test)
 
+#creating dummies for categorical columns
 all_hr = createDummies(all_hr, "sales")
 all_hr = createDummies(all_hr ,"salary")
 
+#Finding NA values
 sum(is.na(all_hr)) # no na values
 colSums(is.na(all_hr))
+#Converting left variable to factor
 all_hr$left = as.numeric(all_hr$left ==1)
 all_hr$left = as.factor(all_hr$left)
 
 
+#Splitting train and test data from combined data
 hr_train = all_hr %>% filter(data == "train") %>% select(-data)
 hr_test = all_hr %>% filter(data =="test") %>% select(-left, -data)
-#---------------------------------------------------------------
- # param = list(interaction.depth = 1:20,
- #              n.trees = c(50,100,200,500,700,300,800,900,1000,
- #                          150,250,350,450,550,650,750),
- #              shrinkage = c(0.1, 0.01, 0.001, 0.0001),
- #              n.minobsinnode = c(1,2,5,10,15,20,25,30,35,40)
- #   )
-# 
-# subset_paras=function(full_list_para,n=10){
-#   all_comb=expand.grid(full_list_para)
-#   s=sample(1:nrow(all_comb),n)
-#   subset_para=all_comb[s,]
-#   return(subset_para)
-# }
-# 
-# num_trials = 100
-# my_params = subset_paras(param,num_trials)
 
+#Building stacking model---------------------------
+#function to create folds
 mykfolds = function(nobs, nfold = 5){
   t = cvFolds(nobs, K = nfold, type = "random")
   folds = list()
@@ -67,6 +57,7 @@ mykfolds = function(nobs, nfold = 5){
 nfold = 100
 myfolds = mykfolds(nrow(hr_train), nfold);
 
+#using only two models heres for stacking
 hr_train_layer = data.frame(rf_var = numeric(nrow(hr_train)),
                             gbm_var = numeric(nrow(hr_train)))
 
@@ -94,9 +85,8 @@ for(i in 1:nfold){
   
 };View(hr_train_layer)
 
-#rf.val = ifelse(rf_score > 0.5, 1, 0)
 
-
+## stack layer 2  data  for test 
 hr_test_layer = data.frame(rf_var = numeric(nrow(hr_test)),
                             gbm_var = numeric(nrow(hr_test)))
 
@@ -122,26 +112,97 @@ test.score_train = predict(log.mod, newdata = hr_train_layer, type ="response")
 
 class.pred_train = as.numeric(test.score_train > 0.5)
 
-#layer_rf = ifelse(hr_test_layer$rf_var > 0.5, 1, 0)
-#layer_gbm =  ifelse(hr_test_layer$gbm_var < 0.5, 1, 0)
-
 table(layer_gbm, layer_rf)
 err = mean(class.pred != hrtestlayer_rf)
 print(paste0("test error ", err))
 
 auc_score=auc(roc(hr_train_layer$left,class.pred_train)); auc_score
 
-write.table(class.pred, "hr5.csv", row.names = F, col.names = "left")
+
+###Random Forest model--------------------------------------------------------------------
+#Paramter list
+param = list(mtry = c(1,2,4,6,8,10,12,14,16,18,20),
+             ntree = c(50,100,500,700,800,900,1000,
+                       #150,250,350,450,550,650,750),
+             ntree = c(500,400,600,700),
+             maxnodes = c(5,10,15,20,30),
+             nodesize = c(1,2,5,15,10))
+
+#subtsetting parameter
+subset_paras = function(full_list_para, n = 10){
+  all_comb = expand.grid(full_list_para)
+  set.seed(128)
+  s = sample(1:nrow(all_comb), n)
+  subset_para = all_comb[s,]
+  return(subset_para)
+}
+  #Cost function
+  mycost_auc = function(y, yhat){
+    roccurve=pROC::roc(y,yhat)
+    score=pROC::auc(roccurve)
+    return(score)
+  }
+  num_trials= 20
+  my_params=subset_paras(param,num_trials)
+  
+  myauc <- 0
+  
+  for(i in 1:num_trials){
+    print(paste('starting iteration :',i))
+    # uncomment the line above to keep track of progress
+    params = my_params[i,]
+    
+    k = cvTuning(randomForest, left~., 
+                 data =train1,
+                 tuning =params,
+                 folds = cvFolds(nrow(train1), K=12, type ="random"),
+                 cost =mycost_auc, 
+                 seed =2,
+                 predictArgs = list(type="prob")
+    )
+    score.this=k$cv[,2]
+   
+    if(score.this>myauc){
+      print(params)
+
+      myauc=score.this
+      print(myauc)
+      best_params=params
+    }
+    
+    print('DONE')
+  };best_params;myauc
+             
+  best_params=data.frame(mtry = 8,
+                         ntree=500,
+                         maxnodes=110,
+                         nodesize=1)
+  
+  ## Model on the entire training data
+  
+  left_new_rf=randomForest(as.factor(left)~.,
+                           mtry=best_params$mtry,
+                           ntree=best_params$ntree,
+                           maxnodes=best_params$maxnodes,
+                           nodesize=best_params$nodesize,
+                           data=train1
+  );left_new_rf
+  
 
 
+train.score = predict(left_new_rf, newdata = train1, type = "prob")
+train.pred = ifelse(train.score> 0.5, 1, 0)
+auc_score=auc(roc(train1$left,train.score[,2])); auc_score
 
-scores12 = View(data.frame( "rf" = layer_rf, 
-                            "lr" = class.pred,"gbm" = layer_gbm, "rfsc"= hr_test_layer$rf_var,
-                             "gbmscore "= hr_test_layer$gbm_var)
-                  )
+val.score = predict(left_new_rf, newdata = train2, type = "prob")
+val.pred = ifelse(val.score > 0.5, 1, 0)
+auc_score=auc(roc(train2$left,val.score[,1])); auc_score
 
 
-modifi = read.csv("train_hr1.csv")
+test.score = predict(left_new_rf, newdata = hr_test, type = "prob")
+test.pred = ifelse(test.score > 0.5, 1, 0)
+
+left = test.pred[,2]
 
 
 
